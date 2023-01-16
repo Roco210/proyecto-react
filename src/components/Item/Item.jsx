6f6aa093a0674a1ed1d567ca479ad060d321e4c3
Ricardo <rickyfernandez210@gmail.com>
2023-01-16 20:42:32 -0300

    cards

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,11 +1,11 @@
 import "./style.css"
 import ItemCount from "../../components/ItemCount/ItemCount"
 const Item = ({producto}) => {
-  console.log(producto.image)
   return ( 
     <li className="card" key={producto.id}>
       <img src={producto.image} alt={producto.name} />
       <div>{producto.name}</div>
+      <button>Detalle</button>
       <div><ItemCount/></div>
     
     </li>
